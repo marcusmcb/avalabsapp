87b2313ba0ff0771bd34d42b440e0fcadfb3e996
Marcus McBride <marcusmcb@gmail.com>
2021-05-11 13:25:50 -0700

    updated app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Ava Labs Sandbox App
         </a>
       </header>
     </div>
