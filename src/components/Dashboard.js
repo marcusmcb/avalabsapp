a75c21ee01b4398d25621b866c153eb1b43bc215
Marcus McBride <marcusmcb@gmail.com>
2021-05-11 19:30:47 -0700

    returning api data to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,21 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const getCoins = () => {    
-        try {
-            fetch('https://api.coingecko.com/api/v3/coins/list')
-                .then((response) => response.json())
-                .then((data) => console.log("DATA? * * * * * * ", data))
-        } catch (err) {
-            console.log(err)
-        } 
-}
+const Dashboard = () => {  
+  const [coinData, setCoinData] = useState({})
+  const [isBusy, setBusy] = useState(true)
+  useEffect(() => {
+    
+    const getCoins = async () => {
+      try {
+        let req = await fetch(
+          'https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=volume_desc&per_page=100&page=1&sparkline=true'
+        )
+        let response = await req.json()
+        return response
+      } catch (err) {
+        console.log('COIN GECKO API ERROR: ', err)
+      }
+    }
+    getCoins().then((data) => {      
+      setCoinData(data)
+      setBusy(false)
+    })
+  }, [])
+  console.log(coinData[1])    
 
-getCoins()
-
-const Dashboard = () => {
   return (
-    <div>
-      <h2>It Works</h2>
+    <div>      
+      {isBusy ? (
+        <p>Loading Coin Data...</p>
+      ) : (       
+        <h5>
+            {coinData.map((coin, i) => <p key={i}>{coin.name} | {coin.symbol.toUpperCase()} | {coin.market_cap_change_24h} 24h vol | {coin.current_price}</p>)}
+        </h5>        
+      )}      
     </div>
   )
 }
