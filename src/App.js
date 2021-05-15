ccfdbb5d27a489cc05c3c0e5b3fa8dd5e2d76263
Marcus McBride <marcusmcb@gmail.com>
2021-05-15 12:01:12 -0700

    updated font

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className='App'>      
         <header className='App-header'>
-          <p>Current CoinGecko Data</p>
+          <p>CoinSnapShot</p>
         </header>
         <Dashboard />      
     </div>
