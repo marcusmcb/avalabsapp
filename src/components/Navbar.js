d01c465f8bc03676579381d4f26d0caf0da324fd
Marcus McBride <marcusmcb@gmail.com>
2021-06-28 14:36:22 -0700

    added navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <div>
+      <header className='navbar'>
+        <p>CoinSnapShot</p>
+      </header>
+    </div>
+  )
+}
+
+export default Navbar
